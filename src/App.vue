af98f1a0b1ed137267ee58476042efd182530aaf
ElisaMavilia <elisamavilia1@gmail.com>
2024-04-09 18:37:54 +0200

    added title

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
-
+      <h1>{{ title }}</h1>
   </div>
 </template>
 
 <script>
   export default {
     data(){
-      
+      return {
+        title: "La mia prima app con Vite!"
+      }
     }
   }
 </script>
